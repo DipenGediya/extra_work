e78e4dc2f71361bc520ef217fe9ce5c952176d98
DipenGediya <141924820+DipenGediya@users.noreply.github.com>
2024-04-22 17:12:34 +0530

    Add files via upload

---
+++ DAS-1.js
@@ -0,0 +1,14 @@
+let arr = [49, 66, 17, 18, 7, 31, 32]
+
+let index = 3;
+
+let value = 45;
+
+for (let i = arr.length - 1; i >= index; i--) {
+    arr[i + 1] = arr[i];
+}
+
+arr[index] = value;
+
+console.log(arr);
+
