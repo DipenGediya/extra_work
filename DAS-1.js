4974774a20bfdc763f08634acfe76f4a060e8c6a
DipenGediya <141924820+DipenGediya@users.noreply.github.com>
2024-04-24 16:46:29 +0530

    second commit

--- DAS-1.js
+++ DAS-1.js
@@ -1,14 +1,29 @@
-let arr = [49, 66, 17, 18, 7, 31, 32]
+//add element 
 
-let index = 3;
+// let arr = [49, 66, 17, 18, 7, 31, 32]
 
-let value = 45;
+// let index = 3;
 
-for (let i = arr.length - 1; i >= index; i--) {
-    arr[i + 1] = arr[i];
+// let value = 45;
+
+// for (let i = arr.length - 1; i >= index; i--) {
+//     arr[i + 1] = arr[i];
+// }
+
+// arr[index] = value;
+
+// console.log(arr);
+
+// remove element
+
+let arr = [49, 66, 17, 18, 7, 31, 32];
+
+let index = 2;
+
+for (let i = index; i < arr.length; i++) {
+    arr[i] = arr[i + 1]
 }
 
-arr[index] = value;
+arr.length = arr.length - 1;
 
 console.log(arr);
-
